601ab0aaab3b208a78ae18ef9e0e7d0c1037e4e3
Qnary <qnary@qnary-VM.(none)>
2012-08-08 14:47:48 -0400

    trying some stuff out

--- test/test.js
+++ test/test.js
@@ -9,3 +9,7 @@ s.get('/v1/segments/1665029/', function(err, resp, data) {
 s.getSegment(1665029, function(err, resp, data) {
   console.log(data);
 });
+
+s.getSegmentEfforts(1665029, {athleteId: 10749}, function(err, resp, data) {
+  console.log(data);
+});
