0019aecb5d1568205cae73e1168bcc3a0eb2f3ed
Qnary <qnary@qnary-VM.(none)>
2012-08-08 16:13:25 -0400

    working out some auth stuff

--- test/test.js
+++ test/test.js
@@ -1,15 +1,26 @@
-var Strava = require('../lib/strava');
+var strava_api = require('../lib/strava');
 
-var s = new Strava();
+var s = new strava_api();
 
 s.get('/v1/segments/1665029/', function(err, resp, data) {
+  if (err) console.log(err);
   console.log(data);
 });
 
-s.getSegment(1665029, function(err, resp, data) {
+s.getSegment(1665029, function(err, data) {
+  if (err) console.log(err);
   console.log(data);
 });
 
-s.getSegmentEfforts(1665029, {athleteId: 10749}, function(err, resp, data) {
+s.getSegmentEfforts(1665029, {athleteId: 10749}, function(err, data) {
+  if (err) console.log(err);
+  console.log(data);
+});
+
+// authenticate
+var sauth = new strava_api({email: '{email}', password:'{password}'});
+
+sauth.getAthlete(10749, function(err, data) {
+  if (err) console.log(err);
   console.log(data);
 });
